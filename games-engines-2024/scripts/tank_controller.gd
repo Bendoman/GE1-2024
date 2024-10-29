bc5fbd3e66646571f1de787ddc80dc28c2c16abb
Ben <Bendavcorr@Gmail.com>
2024-10-29 20:23:44 +0000

    Added collision shapes to objects in tank scene

--- games-engines-2024/scripts/tank_controller.gd
+++ games-engines-2024/scripts/tank_controller.gd
@@ -1,17 +1,24 @@
-extends MeshInstance3D
+extends CharacterBody3D
 
-@export var speed = 20
-@export var turningRotation = 60
+@export var speed = 40
+@export var turningRotation = 200
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 #	Translate tank based on user input
 	var movementAxis = Input.get_axis("move_forward", "move_back")
-	translate(Vector3(0, 0, movementAxis * delta * speed))
+	#translate(Vector3(0, 0, movementAxis * delta * speed))
+	var v = Vector3()
+	v = global_transform.basis.z
+	velocity = v * speed * movementAxis
+	move_and_slide()
+	
 	
 #	Rotates tank based on user input
 	var rotationAxis = Input.get_axis("turn_right", "turn_left")
 	rotate_y(deg_to_rad(turningRotation) * delta * rotationAxis)
+	
+	
