a1ebc1d6824a3920c9491637f5ebe674f93042e7
Ben <Bendavcorr@Gmail.com>
2024-11-05 13:51:13 +0000

    Added vertical and face rotation planes and plane functionality

--- games-engines-2024/scenes/rotation_planes.gd
+++ games-engines-2024/scenes/rotation_planes.gd
@@ -8,20 +8,42 @@ var tweening = false
 var horizontal_plane_index = 0
 var horizontal_planes = ['bottom_horizontal_plane', 'middle_horizontal_plane', 'top_horizontal_plane']
 
+var vertical_plane_index = 0
+var vertical_planes = ['bottom_vertical_plane', 'middle_vertical_plane', 'top_vertical_plane']
+
+var face_plane_index = 0
+var face_planes = ['bottom_face_plane', 'middle_face_plane', 'top_face_plane']
+
+var highlighted_plan_type = 'HORIZONTAL'
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_default_materials()
-	swap_highlighted_plane()
+	swap_highlighted_plane(horizontal_planes, 0)
 	
 func set_default_materials():
 	for plane in horizontal_planes:
 		get_node(plane).set_surface_override_material(0, default_material)
+	for plane in vertical_planes:
+		get_node(plane).set_surface_override_material(0, default_material)
+	for plane in face_planes:
+		get_node(plane).set_surface_override_material(0, default_material)
 
-func swap_highlighted_plane():
-	print(horizontal_planes[horizontal_plane_index])
+func swap_highlighted_plane(planes_array, index):
+	print(planes_array[index])
 	highlighted_plane.set_surface_override_material(0, default_material)
-	get_node(horizontal_planes[horizontal_plane_index]).set_surface_override_material(0, highlighted_material)
-	highlighted_plane = get_node(horizontal_planes[horizontal_plane_index])
+	get_node(planes_array[index]).set_surface_override_material(0, highlighted_material)
+	highlighted_plane = get_node(planes_array[index])
+	var rotationArea:Area3D = highlighted_plane.get_node('RotationArea')
+	
+	for node in rotationArea.get_overlapping_bodies():
+		print(node)
+		var old_rotation = node.global_rotation
+		var old_position = node.global_position
+		node.get_parent().remove_child(node)
+		highlighted_plane.add_child(node)
+		node.global_rotation = old_rotation
+		node.global_position = old_position
 
 func _input(event):
 	#print(event)
@@ -29,13 +51,44 @@ func _input(event):
 		horizontal_plane_index -= 1
 		if(horizontal_plane_index < 0):
 			horizontal_plane_index = len(horizontal_planes) - 1
-		swap_highlighted_plane()
+		swap_highlighted_plane(horizontal_planes, horizontal_plane_index)
+		highlighted_plan_type = 'HORIZONTAL'
 
 	if(Input.is_action_just_pressed("up")):
 		horizontal_plane_index += 1
 		if(horizontal_plane_index > len(horizontal_planes) - 1):
 			horizontal_plane_index = 0	
-		swap_highlighted_plane()
+		swap_highlighted_plane(horizontal_planes, horizontal_plane_index)
+		highlighted_plan_type = 'HORIZONTAL'
+		
+	if(Input.is_action_just_pressed("left")):
+		vertical_plane_index -= 1
+		if(vertical_plane_index < 0):
+			vertical_plane_index = len(vertical_planes) - 1
+		swap_highlighted_plane(vertical_planes, vertical_plane_index)
+		highlighted_plan_type = 'VERTICAL'
+
+	if(Input.is_action_just_pressed("right")):
+		vertical_plane_index += 1
+		if(vertical_plane_index > len(vertical_planes) - 1):
+			vertical_plane_index = 0	
+		swap_highlighted_plane(vertical_planes, vertical_plane_index)
+		highlighted_plan_type = 'VERTICAL'
+
+	if(Input.is_action_just_pressed("in")):
+		face_plane_index += 1
+		if(face_plane_index > len(face_planes) - 1):
+			face_plane_index = 0	
+		swap_highlighted_plane(face_planes, face_plane_index)
+		highlighted_plan_type = 'FACE'
+		
+	if(Input.is_action_just_pressed("out")):
+		face_plane_index -= 1
+		if(face_plane_index < 0):
+			face_plane_index = len(face_planes) - 1
+		swap_highlighted_plane(face_planes, face_plane_index)
+		highlighted_plan_type = 'FACE'
+		
 		
 	if(Input.is_action_just_pressed("turn_clockwise")):
 		if(!tweening):
@@ -51,11 +104,16 @@ func _process(delta):
 
 func tween_rotation(deg):
 	var tween = create_tween()
-	var rotation_amount = Vector3(0, deg, 0)
+	var rotation_amount
+	
+	if(highlighted_plan_type == 'HORIZONTAL'):
+		rotation_amount = Vector3(0, deg, 0)
+	elif(highlighted_plan_type == 'VERTICAL' or highlighted_plan_type == 'FACE'):
+		rotation_amount = Vector3(deg, 0, 0)
 	
 	tweening = true
 	tween.connect("finished", on_tween_finish)
-	tween.tween_property(highlighted_plane, "rotation_degrees", rotation_amount, .5).as_relative().set_trans(Tween.TRANS_SPRING)
+	tween.tween_property(highlighted_plane, "rotation_degrees", rotation_amount, .25).as_relative().set_trans(Tween.TRANS_SPRING)
 	
 func on_tween_finish():
 	tweening = false
